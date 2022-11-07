2fd613c51a6e5ddd70d980160a149e422c7bfd55
Wojtek Buchwald <wojtek.buchwald@windowslive.com>
2022-11-07 23:07:06 +0100

    Init commit

---
+++ BFQCA_Backend/src/test/java/com/example/bfqca_backend/BfqcaBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.bfqca_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BfqcaBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

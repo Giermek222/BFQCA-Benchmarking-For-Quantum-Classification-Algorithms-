a10ec65abbb11d1b081eb549277c8aad06e8013e
Wojtek Buchwald <wojtek.buchwald@windowslive.com>
2022-12-18 23:17:19 +0100

    fix annotation

--- BFQCA_Backend/src/main/java/com/example/bfqca_backend/controllers/AlgorithmController.java
+++ BFQCA_Backend/src/main/java/com/example/bfqca_backend/controllers/AlgorithmController.java
@@ -9,7 +9,12 @@ import com.example.bfqca_backend.utils.mappers.AlgorithmMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestHeader;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
 
 import javax.validation.Valid;
 import java.io.IOException;

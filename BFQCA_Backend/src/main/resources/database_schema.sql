ed8ce456704e39dd186e98d2569f056dcf55d273
Wojtek Buchwald <wojtek.buchwald@windowslive.com>
2022-12-29 19:29:08 +0100

    add deployment info

--- BFQCA_Backend/src/main/resources/database_schema.sql
+++ BFQCA_Backend/src/main/resources/database_schema.sql
@@ -1,32 +1,31 @@
 create table algorithm (
-                           id int not null AUTO_INCREMENT,
+                           id int not null identity,
                            algorithmName varchar(30) not null,
                            problemName varchar(100) not null,
                            algorithmDescription varchar(100),
                            constraint algorithm_pk primary key (id),
-                           constraint algorithm_uk unique key (algorithmName)
-) engine = INNODB;
+);
 
-create table user (
-                      id int not null AUTO_INCREMENT,
+create table users (
+                      id int not null identity ,
                       userName varchar(30) not null,
                       password varchar(100) not null,
                       token varchar(20),
-                      constraint algorithm_pk primary key (id)
-) engine = INNODB;
+                      constraint users_pk primary key (id)
+);
 
 create table benchmark (
-                           id int not null AUTO_INCREMENT,
+                           id int not null identity ,
                            algorithmName varchar(30) not null,
                            problemName varchar(30) not null,
-                           accuracyLearning double,
-                           accuracyTest double,
-                           lossTest double,
-                           lossLearning double,
-                           maxLatency double,
-                           minLatency double,
-                           avgLatency double,
-                           latencyPercentile double,
-                           time double,
-                           constraint algorithm_pk primary key (id)
-) engine = INNODB;
+                           accuracyLearning numeric(9,3),
+                           accuracyTest numeric(9,3),
+                           lossTest numeric(9,3),
+                           lossLearning numeric(9,3),
+                           maxLatency numeric(9,3),
+                           minLatency numeric(9,3),
+                           avgLatency numeric(9,3),
+                           latencyPercentile numeric(9,3),
+                           time numeric(9,3),
+                           constraint benchmark_pk primary key (id)
+);

3954ffe3a7de43a83d84f9fc8d47036bb339b77c
Wojtek Buchwald <wojtek.buchwald@windowslive.com>
2023-01-03 18:22:12 +0100

    add signatures

--- BFQCA_Backend/src/main/java/com/example/bfqca_backend/services/impl/AlgorithmServiceImpl.java
+++ BFQCA_Backend/src/main/java/com/example/bfqca_backend/services/impl/AlgorithmServiceImpl.java
@@ -23,9 +23,9 @@ import java.util.stream.Collectors;
 public class AlgorithmServiceImpl implements AlgorithmService {
 
     static final String rootDirectory =Paths.get(".").toAbsolutePath().getParent().getParent().toAbsolutePath().normalize().toString();
-    private static final String pathToPythonScript = rootDirectory + "\\BFQCA_Cowskit\\hook2.py ";
-    private static final String pathWithAlgorithms = rootDirectory + "\\BFQCA_Cowskit\\ ";
-    private static final String pathToExecutionFolder = rootDirectory + "\\BFQCA_Cowskit ";
+    private static final String pathToPythonScript = rootDirectory + "\\BFQCA_Cowskit\\cowskit_utils\\hook2.py ";
+    private static final String pathWithAlgorithms = rootDirectory + "\\BFQCA_Cowskit\\custom_algorithms\\";
+    private static final String pathToExecutionFolder = rootDirectory + "\\BFQCA_Cowskit";
     @Autowired
     AlgorithmRepository algorithmRepository;
 

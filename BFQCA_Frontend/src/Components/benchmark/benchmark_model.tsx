eebddc64433a19a2b0bf76532090c7d7152fabf2
WFS\babinski <michal.babinski@webfleet.com>
2023-01-23 10:03:54 +0100

    fixes

--- BFQCA_Frontend/src/Components/benchmark/benchmark_model.tsx
+++ BFQCA_Frontend/src/Components/benchmark/benchmark_model.tsx
@@ -1,15 +1,21 @@
 const BenchmarkModel = new Map([
-    ["algorithmName", "Algorithm"],
-    ["problemName", "Problem"],
-    ["accuracyLearning", "Learning Accuracy"],
-    ["accuracyTest", "Test Accuracy"],
-    ["lossLearning", "Learning Loss"],
-    ["lossTest", "Test Loss"],
-    ["time", "Time"],
-    ["maxLatency", "Max Latency"],
-    ["minLatency", "Min Latency"],
-    ["avgLatency", "Avg Latency"],
-    ["latencyPercentile", "Latency Percentile"],
-  ]);
-  
+  ["algorithm_name", "Algorithm"],
+  ["problem_name", "Problem"],
+  ["training_accuracy", "Training Accuracy"],
+  ["training_precision", "Training Precision"],
+  ["training_recall", "Training Recall"],
+  ["training_f1_score", "Training F1 Score"],
+  ["training_loss", "Learning Loss"],
+  ["test_accuracy", "Test Accuracy"],
+  ["test_loss", "Test Loss"],
+  ["test_precision", "Test Precision"],
+  ["test_recall", "Test Recall"],
+  ["test_f1_score", "Test F1 Score"],
+  ["time", "Time"],
+  ["max_latency_ms", "Max Latency"],
+  ["min_latency_ms", "Min Latency"],
+  ["avg_latency_ms", "Avg Latency"],
+  ["percentile_latency_ms", "Latency Percentile"],
+]);
+
 export default BenchmarkModel;
